13eb08447a8b2502602ba16149a70a8bf7c8c12c
강단 <kkangdanni@sk.com>
2021-07-25 19:06:59 +0900

    Js/views 폴더 분리

--- DOWN_TIMER/urls.py
+++ DOWN_TIMER/urls.py
@@ -16,6 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import timer.views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',timer.views.test )
 ]
+
+
+
+
+
