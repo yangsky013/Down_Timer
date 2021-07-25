e24b2c55691c97d3860cd028de19861281804612
yangsky013 <saffron902@naver.com>
2021-07-25 19:11:29 +0900

    list 추가

--- DOWN_TIMER/urls.py
+++ DOWN_TIMER/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from timer import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cycles/', views.cycles)
 ]
--- timer/views.py
+++ timer/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def cycles(request):
+    return render(request, 'cycles.html')
