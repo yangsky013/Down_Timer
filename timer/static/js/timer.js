cf73e67e3bc07330ee3ec2a342f1c172fb759a68
강단 <kkangdanni@sk.com>
2021-08-15 20:09:55 +0900

    timer js update

--- timer/static/js/timer.js
+++ timer/static/js/timer.js
@@ -1,11 +1,15 @@
 const start_btn = document.getElementById("start_btn");
 const timer_head = document.getElementById("timer_head");
 const timer = document.getElementById("timer");
+const totalTimer_head = document.getElementById("totalTime");
 
-var exerciseTime = 10;
-var readyTime = 5;
+var exerciseTime = 5;
+var readyTime = 3;
+var breakTime = 3;
 var round = 1;
 var cycle = 2;
+
+var totalTime = exerciseTime + readyTime + breakTime;
 
 var min = "";
 var sec = "";
@@ -15,9 +19,13 @@ let intervalTime = "";
 
 let isReady = true;
 let isExcercise = false;
+let isBreak = false;
 
 //준비시간 defualt
 timer.innerHTML = "0" + ":" + checkTime(readyTime % 60);
+
+//전체시간 defulat
+totalTimer_head.innerHTML = "0" + ":" + checkTime(totalTime % 60);
 
 function countReadyTimer() {
   intervalTime = setInterval(function () {
@@ -27,10 +35,11 @@ function countReadyTimer() {
     timer.innerHTML = min + ":" + checkTime(sec);
     readyTime--;
 
-    if (readyTime <= 0) {
+    if (readyTime < 0) {
       stopTimer();
 
       isReady = false;
+      isExcercise = true;
 
       startTimer();
     }
@@ -52,6 +61,33 @@ function countExcerciseTimer() {
       stopTimer();
 
       isExcercise = false;
+      isBreak = true;
+
+      startTimer();
+    }
+  }, 1000);
+}
+
+function countBreakTimer() {
+  timer_head.innerHTML = "휴식";
+  isBreak = true;
+
+  intervalTime = setInterval(function () {
+    min = parseInt(breakTime / 60);
+    sec = breakTime % 60;
+
+    timer.innerHTML = min + ":" + checkTime(sec);
+    breakTime--;
+
+    if (breakTime < 0) {
+      stopTimer();
+
+      isBreak = false;
+      isExcercise = true;
+
+      start_btn.innerText = "시작";
+
+      // startTimer();
     }
   }, 1000);
 }
@@ -62,9 +98,15 @@ function startTimer() {
   if (isReady) {
     console.log("isReady");
     countReadyTimer();
-  } else {
+  }
+  if (isExcercise) {
     console.log("is운동");
     countExcerciseTimer();
+    // timer.innerHTML = "종료";
+  }
+  if (isBreak) {
+    console.log("is휴식");
+    countBreakTimer();
     // timer.innerHTML = "종료";
   }
 }
